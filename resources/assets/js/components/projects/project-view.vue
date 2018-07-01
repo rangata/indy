67075c533ca838a9d320276f76d7ea56e9c188d7
Rangata <urandom@abv.bg>
2018-07-01 12:45:16 +0300

    Low

---
+++ resources/assets/js/components/projects/project-view.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "project-view"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: urandom
+ * Date: 01.07.18
+ * Time: 01:08
+ */
